a57ab79cbbc2ad423eb5177fa0f53d3dcb913455
BozidarArsic <arsicbozidar98@gmail.com>
2021-11-09 17:51:21 +0100

    created navbar

---
+++ frontend/eobrazovanje/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+   <div id="nav">
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="4"><v-spacer/></v-col>
+          <v-col cols="12" sm="4">
+                <router-link :to="{ name: 'Courses'}">Courses</router-link> 
+          </v-col>
+          <v-col cols="12" sm="4"/>
+        </v-row>
+      </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+    data(){
+      return {
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
